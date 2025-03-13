68add44c3f0dfbba2df3acbcc8af8e1e34398a8d
launcejoshuadayao <launcejoshuadayao@gmail.com>
2025-03-13 16:54:15 +0800

    3/13/2025lansu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, session, redirect, request, flash
+from flask import Flask, url_for, render_template, session, redirect, request, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -52,8 +52,8 @@ def login():
     return render_template('login.html')
 
 
-#superadmin
-@app.route('/superadmin')
+#superadmin --------------------------------------------------------------------------
+@app.route('/superadmin',  endpoint='superadmin_dashboard')
 def superadmin_dashboard():
     if 'role' in session and session['role'] == 'superadmin':
         user = db.session.get(User, session['user_id'])
@@ -61,12 +61,73 @@ def superadmin_dashboard():
     flash("Unauthorized access!", "danger")
     return redirect(url_for('login'))
 
+@app.route('/superadmin/monitoring', endpoint='superadmin_monitoring')
+def superadmin_monitoring():
+    if 'role' in session and session['role'] == 'superadmin':
+        user = db.session.get(User, session['user_id'])
+        return render_template('superadmin/monitoring.html', username=user.username)
+    flash("Unauthorized access!", "danger")
+    return redirect(url_for('login'))
+
+@app.route('/superadmin/paid-loans')
+def superadmin_paid_loans():
+    if 'role' in session and session['role'] == 'superadmin':
+        user = db.session.get(User, session['user_id'])
+        return render_template('superadmin/paid_loans.html', username=user.username)
+    flash("Unauthorized access!", "danger")
+    return redirect(url_for('login'))
+
+@app.route('/superadmin/overdue-loans')
+def superadmin_overdue_loans():
+    if 'role' in session and session['role'] == 'superadmin':
+        user = db.session.get(User, session['user_id'])
+        return render_template('superadmin/overdue_loans.html', username=user.username)
+    flash("Unauthorized access!", "danger")
+    return redirect(url_for('login'))
+
+@app.route('/superadmin/activity-logs')
+def superadmin_activity_logs():
+    if 'role' in session and session['role'] == 'superadmin':
+        user = db.session.get(User, session['user_id'])
+        return render_template('superadmin/logs.html', username=user.username)
+    flash("Unauthorized access!", "danger")
+    return redirect(url_for('login'))
+
+@app.route('/superadmin/manage-users')
+def superadmin_manage_users():
+    if 'role' in session and session['role'] == 'superadmin':
+        user = db.session.get(User, session['user_id'])
+        return render_template('superadmin/manage_users', username=user.username)
+    flash("Unauthorized access!", "danger")
+    return redirect(url_for('login'))
+
+#admin --------------------------------------------------------------------------------------
 @app.route('/admin')
 def admin_dashboard():
     if 'role' in session and session['role'] == 'admin':
         return render_template('admin/admin_dashboard.html')
     flash("Unauthorized access!", "danger")
     return redirect(url_for('login'))
+
+
+#addloan -----------------------------
+@app.route('/add-loan', methods=['POST'])
+def add_loan():
+    lender_name = request.form.get('lender_name')
+    amount = request.form.get('amount')
+    application_date = request.form.get('application_date')
+    months_to_pay = request.form.get('months_to_pay')
+    monthly_payment = float(amount) / int(months_to_pay)
+
+    sql = "INSERT INTO loans (lender_name, amount, application_date, months_to_pay, monthly_payment) VALUES (%s, %s, %s, %s, %s)"
+    values = (lender_name, amount, application_date, months_to_pay, monthly_payment)
+
+    cursor = db.connection.cursor()
+    cursor.execute(sql, values)
+    db.connection.commit()
+    cursor.close()
+
+    return jsonify({'success': True})
 
 @app.route('/logout')
 def logout():
